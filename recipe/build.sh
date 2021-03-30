15bbc981f263bbc1799af58aff169a76c722b8a9
Maksim Rakitin <mrakitin@bnl.gov>
2021-03-29 22:49:45 -0400

    Sync with CF

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,27 @@
-./configure --prefix=$PREFIX
+#!/usr/bin/env bash
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build-aux
+set -ex
+
+if [[ "$target_platform" == linux* ]]; then
+  rm -f tests/scripts/functions/wildcard
+elif [[ "$target_platform" == osx* ]]; then
+  rm -f tests/scripts/variables/INCLUDE_DIRS
+fi
+
+if [[ ${target_platform} =~ .*aarch64.* || ${target_platform} =~ .*ppc64le.* ]]; then
+  config_opts="--disable-dependency-tracking"
+else
+  config_opts=""
+fi
+
+./configure --prefix=$PREFIX ${config_opts}
 # bootstrap building make without make
-sh build.sh
+bash build.sh
 # make
-./make check
-./make install
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  ./make check
+  ./make install
+else
+  make install
+fi
